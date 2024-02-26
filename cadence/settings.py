28dbbdfb2f084f0450defcec80e4b965d681a247
Ivan Krause <ivan.krause@gmail.com>
2024-02-26 23:12:24 +0000

    test collectstatic command 2

--- cadence/settings.py
+++ cadence/settings.py
@@ -152,7 +152,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
@@ -160,18 +159,19 @@ AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
 
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfilesbeta')
 
 # For development
 if DEBUG:
     STATICFILES_DIRS = [BASE_DIR / "static"]
     STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
     STATIC_URL = "static/"
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # For production
 else:
     STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/'
+    STATIC_URL = f'https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/cadence/'
+
 
 
 # Default primary key field type
